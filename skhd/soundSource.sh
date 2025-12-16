126285b48b5d4df226c82b5b57fccb9b48ad4283
capitanBOBO <savin.capitanbobo@gmail.com>
2025-12-16 14:25:18 +0200

    some cleaning, added borders and mc

--- skhd/soundSource.sh
+++ skhd/soundSource.sh
@@ -1,9 +1,8 @@
 #! /bin/zsh
 
-source ~/.config/aerospace/scripts/alias.sh
 
 SOURCES=("${(@f)$(SwitchAudioSource -a -t output -f json | jq  -r '"\(.id)\t\t\(.name)"')}")
 
-SELECTED_SOURCE_ID=$(printf "%s\n" "${SOURCES[@]}" | choose_base -p "Audio OUTPUT" | cut -f 1)
+SELECTED_SOURCE_ID=$(printf "%s\n" "${SOURCES[@]}" | choose -p "Audio OUTPUT" | cut -f 1)
 
 SwitchAudioSource -i $SELECTED_SOURCE_ID
